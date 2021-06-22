2fa7d43381b614f812f096f375bf46dcd2c8a63d
Francisco Mejia <frankie@Franciscos-MacBook-Pro.local>
2021-06-21 21:25:52 -0400

    added Cross Origins

--- src/main/java/com/example/springbootbackend/controller/EmployeeController.java
+++ src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package com.example.springbootbackend.controller;
 import com.example.springbootbackend.model.Employee;
 import com.example.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000" )
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -28,3 +30,4 @@ public class EmployeeController {
 //We create REST API here. The standard in RequestMapping is by declaring version No.
 //We will then inject employee repository within the EmployeeController class. The controller class is here.
 //essentially, the database URL and requests are made here.
+//Added cross origins annotation above the rest controller and added the local host url
