caba3155cae127b15bb6200d39d64df72c202c5f
Francisco Mejia <frankie@Franciscos-MacBook-Pro.local>
2021-06-24 00:14:50 -0400

    resolved issue in PostMan

--- src/main/java/com/example/springbootbackend/repository/EmployeeRepository.java
+++ src/main/java/com/example/springbootbackend/repository/EmployeeRepository.java
@@ -11,3 +11,4 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 //By creating this interface and having the repository extend JpaRepository, we can use database methods and provides
 //implementation for all the database methods such as findAll, pagination, etc. (CRUD methods)
 //will now make API call to front end
+//Resolved issue in PostMan
